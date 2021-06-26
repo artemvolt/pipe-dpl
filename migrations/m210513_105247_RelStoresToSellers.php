2cfc91cca6e4983969c2be3b39a0b1fde7682e04
Pozitronik <antikillerxxl@gmail.com>
2021-06-26 21:39:54 +0300

    namespace changed

--- migrations/m210513_105247_RelStoresToSellers.php
+++ migrations/m210513_105247_RelStoresToSellers.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types = 1);
-use pozitronik\core\models\RelationMigration;
+use pozitronik\relations\models\RelationMigration;
 
 /**
  * Class m210513_105247_RelStoresToSellers
