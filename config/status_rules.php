ced1fe09d29b67d2a2a18551d92b9424b451b423
Artem <artem@artwake.ru>
2021-06-29 13:04:29 +0300

    Issues #52. Add statuses for seller, rewards. Move constants to class.

--- config/status_rules.php
+++ config/status_rules.php
@@ -4,6 +4,10 @@ declare(strict_types = 1);
 /*При наличии одноимённого файла в подкаталоге /local конфигурация будет взята оттуда*/
 if (file_exists($localConfig = __DIR__.DIRECTORY_SEPARATOR.'local'.DIRECTORY_SEPARATOR.basename(__FILE__))) return require $localConfig;
 
+use app\models\reward\Rewards;
+use app\models\seller\Sellers;
+use app\models\sys\users\Users;
+
 const CREATED = 1;//создан
 const SENT = 2;//отправлен
 const RECEIVED = 3;//получен
@@ -16,5 +20,34 @@ const ARCHIVE = 7;//в архиве
  * todo: Мультиобработчик для применения статусов
 */
 return [
+	Rewards::class => [
+		CREATED => [
+			'name' => 'Создан',
+			'initial' => true,
+			'finishing' => false,
+			'next' => [SENT, RECEIVED],
+			'allowed' => false
 
+		],
+		SENT => [
+			'name' => 'Отправлен',
+			'initial' => false,
+			'finishing' => false,
+			'next' => [RECEIVED],
+			'allowed' => static function(Rewards $model, Users $user):bool {
+				return true;
+			}
+		],
+		RECEIVED => [
+			'name' => 'Получен',
+			'initial' => false,
+			'finishing' => true,
+			'next' => [],
+			'allowed' => static function(Rewards $model, Users $user):bool {
+				return true;
+			},
+			'color' => '#ff0000'
+		],
+	],
+	Sellers::class => Sellers::class.'::getStatusConfig'
 ];
--- models/seller/Sellers.php
+++ models/seller/Sellers.php
@@ -7,9 +7,16 @@ use app\controllers\SellersController;
 use app\models\addresses\active_record\AddressesAR;
 use app\models\addresses\Addresses;
 use app\models\common\traits\CreateAddressTrait;
+use app\models\phones\Phones;
 use app\models\seller\active_record\SellersAR;
 use app\models\common\traits\CreateAccessTrait;
+use app\models\sys\users\Users;
+use app\models\sys\ValidateException;
+use app\modules\status\models\Status;
+use DomainException;
+use InvalidArgumentException;
 use pozitronik\filestorage\traits\FileStorageTrait;
+use yii\base\InvalidConfigException;
 use yii\helpers\ArrayHelper;
 
 /**
@@ -30,6 +37,12 @@ class Sellers extends SellersAR {
 	use FileStorageTrait;
 	use CreateAccessTrait;
 	use CreateAddressTrait;
+
+	public const SELLER_NOT_ACTIVE = 1;
+	public const SELLER_ACTIVE = 2;
+	public const SELLER_LIMITED = 3;
+	public const SELLER_BLOCKED = 4;
+	public const SELLER_SUSPENDED = 5;
 
 	public $passportTranslation;
 	public $migrationCard;
@@ -76,4 +89,137 @@ class Sellers extends SellersAR {
 		return $this->addressInstance;
 	}
 
+	/**
+	 * @return Phones[]
+	 */
+	public function getPhonesNumbers():array {
+		if (null === $user = $this->relatedUser) {
+			return [];
+		}
+
+		return $user->relatedPhones;
+	}
+
+	/**
+	 * @param string $surname
+	 * @param string $name
+	 * @param string $patronymic
+	 * @param string $phoneNumber
+	 * @param string $email
+	 * @return Sellers
+	 * @throws ValidateException
+	 */
+	public function registerMini(
+		string $surname,
+		string $name,
+		string $patronymic,
+		string $phoneNumber,
+		string $email
+	):self {
+		$self = new self();
+		$self->scenario = self::SCENARIO_REGISTER_MINI;
+		$self->login = $phoneNumber;
+		$self->surname = $surname;
+		$self->name = $name;
+		$self->patronymic = $patronymic;
+		$self->email = $email;
+		if (!$self->save()) {
+			throw new ValidateException($self->getErrors());
+		}
+
+		$self->createAccess();
+		if ([] !== $self->registrationErrors) {
+			throw new DomainException("Не получилось создать пользователя");
+		}
+
+		$self->changeStatus(self::SELLER_NOT_ACTIVE);
+		return $self;
+	}
+
+	public function changeStatus(int $status):void {
+		if (!$this->isExistStatus($status)) {
+			throw new InvalidArgumentException("Неизвестный статус");
+		}
+		$isSaved = Status::setCurrentStatus($this, $status);
+		if (false === $isSaved) {
+			throw new DomainException("Не получилось установить статус для продавца");
+		}
+	}
+
+	/**
+	 * @param int $status
+	 * @return bool
+	 */
+	public function isExistStatus(int $status):bool {
+		return array_key_exists($status, array_keys(self::getStatusConfig()));
+	}
+
+	/**
+	 * @return bool
+	 * @throws InvalidConfigException
+	 */
+	public function isUnActiveStatus():bool {
+		$existentStatus = Status::getCurrentStatus($this);
+		if (null === $existentStatus) {
+			return false;
+		}
+		return $existentStatus === self::SELLER_NOT_ACTIVE;
+	}
+
+	public static function getStatusConfig():array {
+		return [
+			self::SELLER_NOT_ACTIVE => [
+				'id' => self::SELLER_NOT_ACTIVE,
+				'name' => 'Не активирован',
+				'initial' => true,
+				'finishing' => false,
+				'next' => [Sellers::SELLER_ACTIVE, Sellers::SELLER_LIMITED, Sellers::SELLER_BLOCKED, Sellers::SELLER_SUSPENDED],
+				'allowed' => false
+			],
+			self::SELLER_ACTIVE => [
+				'id' => self::SELLER_ACTIVE,
+				'name' => 'Активирован',
+				'initial' => false,
+				'finishing' => true,
+				'next' => [self::SELLER_NOT_ACTIVE, self::SELLER_LIMITED, self::SELLER_BLOCKED, self::SELLER_SUSPENDED],
+				'allowed' => static function(Sellers $model, Users $user):bool {
+					return true;
+				},
+				'style' => 'background: #ffa700; color:black'//стили можно задавать напрямую
+			],
+			self::SELLER_LIMITED => [
+				'id' => Sellers::SELLER_LIMITED,
+				'name' => 'Ограничен',
+				'initial' => false,
+				'finishing' => true,
+				'next' => [self::SELLER_NOT_ACTIVE, self::SELLER_ACTIVE, self::SELLER_BLOCKED, self::SELLER_SUSPENDED],
+				'allowed' => static function(Sellers $model, Users $user):bool {
+					return true;
+				},
+				'color' => '#00ff00'
+			],
+			self::SELLER_BLOCKED => [
+				'id' => Sellers::SELLER_BLOCKED,
+				'name' => 'Заблокирован',
+				'initial' => false,
+				'finishing' => true,
+				'next' => [self::SELLER_NOT_ACTIVE, self::SELLER_ACTIVE, self::SELLER_LIMITED, self::SELLER_SUSPENDED],
+				'allowed' => static function(Sellers $model, Users $user):bool {
+					return true;
+				},
+				'color' => '#00ff00'
+			],
+			self::SELLER_SUSPENDED => [
+				'id' => Sellers::SELLER_SUSPENDED,
+				'name' => 'Suspend',
+				'initial' => false,
+				'finishing' => true,
+				'next' => [self::SELLER_NOT_ACTIVE, self::SELLER_ACTIVE, self::SELLER_LIMITED, self::SELLER_BLOCKED],
+				'allowed' => static function(Sellers $model, Users $user):bool {
+					return true;
+				},
+				'color' => '#00ff00'
+			]
+		];
+	}
 }
