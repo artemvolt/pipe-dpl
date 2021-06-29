78e546b120783559a722f3b85fa62f3c69559775
Artem <artem@artwake.ru>
2021-06-29 13:39:36 +0300

    Issues #52. Get Rewards array constants.

--- config/status_rules.php
+++ config/status_rules.php
@@ -6,48 +6,8 @@ if (file_exists($localConfig = __DIR__.DIRECTORY_SEPARATOR.'local'.DIRECTORY_SEP
 
 use app\models\reward\Rewards;
 use app\models\seller\Sellers;
-use app\models\sys\users\Users;
 
-const CREATED = 1;//создан
-const SENT = 2;//отправлен
-const RECEIVED = 3;//получен
-const DENIED = 4;//отказ поставки
-const CANCELLED = 5;//отмена заказа отправителем
-const DONE = 6;//полностью обработано
-const ARCHIVE = 7;//в архиве
-
-/*
- * todo: Мультиобработчик для применения статусов
-*/
 return [
-	Rewards::class => [
-		CREATED => [
-			'name' => 'Создан',
-			'initial' => true,
-			'finishing' => false,
-			'next' => [SENT, RECEIVED],
-			'allowed' => false
-
-		],
-		SENT => [
-			'name' => 'Отправлен',
-			'initial' => false,
-			'finishing' => false,
-			'next' => [RECEIVED],
-			'allowed' => static function(Rewards $model, Users $user):bool {
-				return true;
-			}
-		],
-		RECEIVED => [
-			'name' => 'Получен',
-			'initial' => false,
-			'finishing' => true,
-			'next' => [],
-			'allowed' => static function(Rewards $model, Users $user):bool {
-				return true;
-			},
-			'color' => '#ff0000'
-		],
-	],
+	Rewards::class => Rewards::class.'::status_config',
 	Sellers::class => Sellers::class.'::getStatusConfig'
 ];
