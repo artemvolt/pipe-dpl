81b778c8db7ed5302a1eb5e364b5edc29c9f946a
suares <pepe_botika69@yahoo.com>
2021-07-02 16:01:49 +0300

    issue #60- Add config for module

---
+++ modules/recogdol/config/config.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+
+/*При наличии одноимённого файла в подкаталоге /local конфигурация будет взята оттуда*/
+if (file_exists($localConfig = __DIR__.DIRECTORY_SEPARATOR.'local'.DIRECTORY_SEPARATOR.basename(__FILE__))) {
+	return require $localConfig;
+}
+
+return [
+	'params' => [
+		'connection' => [
+			'host' => '',
+		]
+	]
+];
