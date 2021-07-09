46a9116b9bf916a6bde2fa412716209d1d23f4af
suares <pepe_botika69@yahoo.com>
2021-07-09 16:07:16 +0300

    history module - Add pozitronik widget

--- modules/history/views/index/index.php
+++ modules/history/views/index/index.php
@@ -12,6 +12,7 @@ use app\modules\history\models\ActiveRecordHistory;
 use app\modules\history\models\HistoryEventInterface;
 use app\modules\history\models\HistorySearch;
 use kartik\datetime\DateTimePicker;
+use pozitronik\grid_config\GridConfig;
 use pozitronik\widgets\BadgeWidget;
 use yii\data\ActiveDataProvider;
 use yii\grid\DataColumn;
@@ -23,111 +24,113 @@ use pozitronik\helpers\Utils;
 
 ?>
 
-<?= GridView::widget([
-	'dataProvider' => $dataProvider,
-	'filterModel' => $searchModel,
-	'summary' => false,
-	'showOnEmpty' => false,
-	'resizableColumns' => true,
-	'responsive' => true,
-	'export' => false,
-	'panel' => [
-		'heading' => $this->title.(($dataProvider->totalCount > 0)?" (".Utils::pluralForm($dataProvider->totalCount, ['запись', 'записи', 'записей']).")":" (нет записей)"),
-	],
-	'formatter' => [
-		'class' => Formatter::class,
-		'nullDisplay' => ''
-	],
-	'columns' => [
-		'id',
-		[
-			'attribute' => 'user',
-			'format' => 'raw',
-			'value' => static function(ActiveRecordHistory $model):string {
-				return BadgeWidget::widget([
-					'items' => $model->relatedUser,
-					'subItem' => 'id',
-					'useBadges' => false,
-					'urlScheme' => [
-						UsersController::to(
-							'index',
-							['UsersSearch[id]' => $model->relatedUser->id??null]
-						)
-					]
-				]);
-			}
+<?= GridConfig::widget([
+	'id' => "{$searchModel->formName()}-index-grid",
+	'grid' => GridView::begin([
+		'dataProvider' => $dataProvider,
+		'filterModel' => $searchModel,
+		'summary' => false,
+		'showOnEmpty' => false,
+		'resizableColumns' => true,
+		'responsive' => true,
+		'export' => false,
+		'panel' => [
+			'heading' => $this->title.(($dataProvider->totalCount > 0)?" (".Utils::pluralForm($dataProvider->totalCount, ['запись', 'записи', 'записей']).")":" (нет записей)"),
 		],
-		[
-			'attribute' => 'event',
-			'value' => static function(ActiveRecordHistory $model) {
-				return $model->historyEvent->eventCaption;
-			},
-			'format' => 'raw',
-			'filter' => HistoryEventInterface::EVENT_TYPE_FRAMEWORK_NAMES,
-			'filterWidgetOptions' => [
-				'pluginOptions' => ['allowClear' => true, 'placeholder' => '']
-			]
+		'formatter' => [
+			'class' => Formatter::class,
+			'nullDisplay' => ''
 		],
-		[
-			'class' => DataColumn::class,
-			'attribute' => 'tag',
-		],
-		[
-			'attribute' => 'at',
-			'filterType' => DateTimePicker::class,
-			'filterWidgetOptions' => [
-				'type' => DateTimePicker::TYPE_INPUT,
-				'pluginOptions' => [
-					'alwaysShowCalendars' => true
+		'columns' => [
+			'id',
+			[
+				'attribute' => 'user',
+				'format' => 'raw',
+				'value' => static function(ActiveRecordHistory $model):string {
+					return BadgeWidget::widget([
+						'items' => $model->relatedUser,
+						'subItem' => 'id',
+						'useBadges' => false,
+						'urlScheme' => [
+							UsersController::to(
+								'index',
+								['UsersSearch[id]' => $model->relatedUser->id??null]
+							)
+						]
+					]);
+				}
+			],
+			[
+				'attribute' => 'event',
+				'value' => static function(ActiveRecordHistory $model) {
+					return $model->historyEvent->eventCaption;
+				},
+				'format' => 'raw',
+				'filter' => HistoryEventInterface::EVENT_TYPE_FRAMEWORK_NAMES,
+				'filterWidgetOptions' => [
+					'pluginOptions' => ['allowClear' => true, 'placeholder' => '']
 				]
-			]
-		],
-		[
-			'attribute' => 'model_class',
-			'value' => static function(ActiveRecordHistory $model) {
-				return null === $model->model_key?$model->model_class:Html::a($model->model_class, ['show', 'for' => $model->model_class, 'id' => $model->model_key]);
-			},
-			'format' => 'raw',
-		],
-		[
-			'class' => DataColumn::class,
-			'attribute' => 'relation_model',
-			'format' => 'raw',
-		],
-		[
-			'attribute' => 'model_key',
-			'value' => static function(ActiveRecordHistory $model) {
-				return null === $model->model_key?$model->model_key:Html::a($model->model_key, ['history', 'for' => $model->model_class, 'id' => $model->model_key]);
-			},
-			'format' => 'raw'
-
-		],
-		[
-			'attribute' => 'actions',
-			'filter' => false,
-			'format' => 'raw',
-			'value' => static function(ActiveRecordHistory $model) {
-				return $model->historyEvent->timelineEntry->content;
-			}
-		],
-		'scenario',
-		[
-			'attribute' => 'delegate',
-			'format' => 'raw',
-			'value' => static function(ActiveRecordHistory $model):string {
-				return BadgeWidget::widget([
-					'items' => $model->relatedUserDelegated,
-					'subItem' => 'id',
-					'useBadges' => false,
-					'urlScheme' => [
-						UsersController::to(
-							'index',
-							['UsersSearch[id]' => $model->relatedUserDelegated->id??null]
-						)
+			],
+			[
+				'class' => DataColumn::class,
+				'attribute' => 'tag',
+			],
+			[
+				'attribute' => 'at',
+				'filterType' => DateTimePicker::class,
+				'filterWidgetOptions' => [
+					'type' => DateTimePicker::TYPE_INPUT,
+					'pluginOptions' => [
+						'alwaysShowCalendars' => true
 					]
-				]);
-			}
+				]
+			],
+			[
+				'attribute' => 'model_class',
+				'value' => static function(ActiveRecordHistory $model) {
+					return null === $model->model_key?$model->model_class:Html::a($model->model_class, ['show', 'for' => $model->model_class, 'id' => $model->model_key]);
+				},
+				'format' => 'raw',
+			],
+			[
+				'class' => DataColumn::class,
+				'attribute' => 'relation_model',
+				'format' => 'raw',
+			],
+			[
+				'attribute' => 'model_key',
+				'value' => static function(ActiveRecordHistory $model) {
+					return null === $model->model_key?$model->model_key:Html::a($model->model_key, ['history', 'for' => $model->model_class, 'id' => $model->model_key]);
+				},
+				'format' => 'raw'
+			],
+			[
+				'attribute' => 'actions',
+				'filter' => false,
+				'format' => 'raw',
+				'value' => static function(ActiveRecordHistory $model) {
+					return $model->historyEvent->timelineEntry->content;
+				}
+			],
+			'scenario',
+			[
+				'attribute' => 'delegate',
+				'format' => 'raw',
+				'value' => static function(ActiveRecordHistory $model):string {
+					return BadgeWidget::widget([
+						'items' => $model->relatedUserDelegated,
+						'subItem' => 'id',
+						'useBadges' => false,
+						'urlScheme' => [
+							UsersController::to(
+								'index',
+								['UsersSearch[id]' => $model->relatedUserDelegated->id??null]
+							)
+						]
+					]);
+				}
+			]
 		]
-	]
+	])
 ]) ?>
 
